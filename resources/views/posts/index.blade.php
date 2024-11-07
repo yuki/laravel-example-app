f7dc6169453142664952d1d2a40eccbbbc3a1c89
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-11-07 17:09:47 +0100

    Post: Añadida la función destroy y modificada la vista index
    - Modificado el controller para añadir la función de destroy
    - Modificada la vista index para añadir botones para editar y borrar

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,9 +8,21 @@
     {{--esto es un comentario: recorremos el listado de posts--}}
     @foreach ($posts as $post)
       {{-- visualizamos los atributos del objeto --}}
-      <li>
-        <a href="{{route('posts.show',$post)}}"> {{$post->titulo}}</a>.
-        Escrito el {{$post->created_at}}
+      <li class="pt-1">
+        <div class="d-flex flex-row">
+          <a href="{{route('posts.show',$post)}}"> {{$post->titulo}}</a>.
+          Escrito el {{$post->created_at}}
+          <a class="btn btn-warning btn-sm" href="{{route('posts.edit',$post)}}"
+            role="button">Editar</a>
+
+          <form action="{{route('posts.destroy',$post)}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-sm btn-danger" type="submit"
+              onclick="return confirm('Are you sure?')">Delete
+            </button>
+          </form>
+        </div>
       </li>
     @endforeach
   </ul>
