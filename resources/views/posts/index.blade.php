5b97754c47029af3abadcc596706c8ed735c6364
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-11-07 17:35:59 +0100

    Modificada vista por sistema de autenticación
    Modificada la vista "index" para que si no estás autenticado, no muestre los botones

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,16 +12,21 @@
         <div class="d-flex flex-row">
           <a href="{{route('posts.show',$post)}}"> {{$post->titulo}}</a>.
           Escrito el {{$post->created_at}}
-          <a class="btn btn-warning btn-sm" href="{{route('posts.edit',$post)}}"
+
+          @auth
+            <a class="btn btn-warning btn-sm" href="{{route('posts.edit',$post)}}"
             role="button">Editar</a>
 
-          <form action="{{route('posts.destroy',$post)}}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button class="btn btn-sm btn-danger" type="submit"
-              onclick="return confirm('Are you sure?')">Delete
-            </button>
-          </form>
+            <form action="{{route('posts.destroy',$post)}}" method="POST"
+              enctype="multipart/form-data">
+              @csrf
+              @method('DELETE')
+              <button class="btn btn-sm btn-danger" type="submit"
+                onclick="return confirm('Are you sure?')">Delete
+              </button>
+            </form>
+          @endauth
+          
         </div>
       </li>
     @endforeach
