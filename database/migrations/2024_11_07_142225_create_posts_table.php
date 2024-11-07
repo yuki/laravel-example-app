8b8319430c6fc8ddac6156c082f09bee6a820d74
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-11-07 15:28:25 +0100

    Post: modificado el migrate
    Añadidos los campos que va a tener el modelo.

--- database/migrations/2024_11_07_142225_create_posts_table.php
+++ database/migrations/2024_11_07_142225_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo',length:255);
+            $table->text('texto');
+            $table->boolean('publicado');
             $table->timestamps();
         });
     }
