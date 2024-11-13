5f6c7f64c9ea3d7c6a80bf07683333d7f45c3855
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-11-13 15:18:36 +0100

    Post: Añadir botón para crear posts
    Añadido botón para crear posts en el posts.index, que se me había olvidado

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,11 @@
 <div class="container">
   <h1>Posts</h1>
 
+  @auth
+  <a class="btn btn-primary btn-sm" href="{{route('posts.create')}}"
+    role="button">Crear post</a>
+  @endauth
+
   <ul>
     {{--esto es un comentario: recorremos el listado de posts--}}
     @foreach ($posts as $post)
